a8cf49b7d52ccbff3c9ed820a46fe2baa5af903b
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-09-13 18:03:28 +0600

    init Case Stmt

--- AdvanceddbQueries - tbl_student-course-enrolled.sql
+++ AdvanceddbQueries - tbl_student-course-enrolled.sql
@@ -232,5 +232,28 @@ INSERT into #tmpstdtbl VALUES(2, 'Sanaul Haq');
 -- Case Statement
 -- """""""""""""""
 -- Case Statement helps in multi-way decision making
+-- Like:
+/* If 1 --> Then Tea
+   If 2 --> Then Coffee
+..
+*/
 
+-- Syntax For Case Statement: 
+/* CASE
+      WHEN condition1 THEN result1
+      WHEN condition2 THEN result2
+      ..
+      ELSE result
+  END;
+*/
+-- Example of Case Statement:
+SELECT *, grade = 
+case
+when gpa=4 then 'A+'
+when gpa>3.5 then 'A'
+when gpa<3.5 then 'A-'
+else 'Missing'
+END
+FROM Student
+GO
 
